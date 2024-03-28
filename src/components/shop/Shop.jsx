051ae1461c100858287aaca7e8dee3098e9728de
Shovan Mazumder <shovan2192@gmail.com>
2024-03-28 18:28:39 +0600

    db added

--- src/components/shop/Shop.jsx
+++ src/components/shop/Shop.jsx
@@ -2,37 +2,44 @@ import React, { useEffect, useState } from 'react';
 import './Shop.css'
 import Product from '../Product/Product';
 import Cart from '../cart/Cart';
+import { addToDb, getShoppingCart } from '../../utilities/fakedb';
 
 const Shop = () => {
     const [products, setProducts] = useState([]);
     const [cart, setCart] = useState([]);
-  
-    
 
-    useEffect( () =>{
-    fetch('products.json')
-    .then(res => res.json())
-    .then(data => setProducts(data))
+
+
+    useEffect(() => {
+        fetch('products.json')
+            .then(res => res.json())
+            .then(data => setProducts(data))
     }, []);
 
-    const handelAddToCart = (product) => {
-        const newCart = [...cart,product];
+    useEffect(() =>{
+        const storedCart = getShoppingCart();
+        console.log(storedCart);
+    },[])
 
-        setCart(newCart); 
-    } 
+    const handelAddToCart = (product) => {
+        const newCart = [...cart, product];
+
+        setCart(newCart);
+        addToDb(product.id);
+    }
     return (
         <div className='shop-container'>
-           <div className='products-container'>
-            {
-                products.map(product => <Product
-                     key={product.id}
-                     product={product}
-                     handelAddToCart={handelAddToCart}
-                     ></Product>)
-            }
-            </div> 
+            <div className='products-container'>
+                {
+                    products.map(product => <Product
+                        key={product.id}
+                        product={product}
+                        handelAddToCart={handelAddToCart}
+                    ></Product>)
+                }
+            </div>
             <div className='cart-container'>
-              <Cart cart ={cart}></Cart>
+                <Cart cart={cart}></Cart>
             </div>
         </div>
     );
